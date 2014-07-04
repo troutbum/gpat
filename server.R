e3f9ca421b7d8d9154f93adf5a6936f4d7a1717b
troutbum <genec2008@gmail.com>
2014-07-04 16:58:10 -0700

    Successful ToDo list plot
    uploaded to shiny apps but error can't connect
    network congestion?

--- server.R
+++ server.R
@@ -6,7 +6,7 @@ library(RColorBrewer)
 colors1 <- brewer.pal(9, "Greens")
 palette1 <- colorRampPalette(colors1)
 colors2 <- brewer.pal(9, "YlOrRd")
-palette2 <- colorRampPalette
+palette2 <- colorRampPalette(colors2)
 
 shinyServer(function(input, output) {
         
@@ -32,8 +32,19 @@ shinyServer(function(input, output) {
                                   input$power,        
                                   input$stamina,
                                   input$strength,
-                                  input$speed), stringsAsFactors=FALSE)               
+                                  input$speed), stringsAsFactors=FALSE)
                 
+                # create a TO DO list, inverse of test results
+                todo <- datasetInput
+                todo[,2] <- abs(todo[,2] - 10)
+                
+                # if a Score is 0, make it a 1
+                for (i in 1:nrow(todo)) 
+                {
+                        if (todo[i,2] == 0)  {todo[i,2] <- todo[i,2] + 1}
+                }
+                
+                # plot datasetInput
                 p <- ggplot(datasetInput, aes(Name, Value, fill = factor(Value))) + 
                         geom_bar(stat="identity") + 
                         coord_polar() +
@@ -44,7 +55,22 @@ shinyServer(function(input, output) {
                         labs(title="") +     
                         scale_fill_manual(values=palette1(10)) +
                         theme(plot.title = element_text(size = rel(2), face="bold"))
-                print(p)
+                #print(p)
+                
+                todoplot <- ggplot(todo, aes(Name, Value, fill = factor(Value))) + 
+                        geom_bar(stat="identity") + 
+                        coord_polar() +
+                        theme(legend.position="none") +
+                        labs(x="To Do List") + 
+                        labs(y="") + 
+                        scale_y_continuous(limits=c(0,10)) +
+                        labs(title="") +     
+                        scale_fill_manual(values=palette2(10)) +
+                        theme(plot.title = element_text(size = rel(2), face="bold"))
+                #print(todoplot)
+                
+                source('~/CourseraHW/R_functions/multiplot.R')
+                print(multiplot(p, todoplot, cols=2))
                 
                 })
         height=700
